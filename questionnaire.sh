3b9c74cad80527168f9deadeac0aae4070e6407e
Biplob14 <biploburrahaman@gmail.com>
2022-11-22 00:43:16 +0600

    FCC files included

---
+++ questionnaire.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# program to read and print some question to shwo basic R/W operations
+echo -e "\n~~ Questionnaire ~~\n"
+
+QUESTION1="What's your name?"
+QUESTION2="Where are you from?"
+QUESTION3="What's your favorite coding website?"
+
+echo $QUESTION1
+read NAME
+
+echo $QUESTION2
+read LOCATION
+
+echo $QUESTION3
+read WEBSITE
+
+echo -e "\nHello $NAME from $LOCATION. I learned that your favorite coding website is $WEBSITE!"
